33d9f7692afb2a74d282b980016fdc486f1576cb
michelnowak-thales <michel.nowak@thalesgroup.com>
2026-03-12 09:01:18 +0100

    Adding sample request script

---
+++ examples/visit_cities/sample_requests.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas
+
+
+def sample_requests(requests, num_requests):
+
+        vals = np.array(requests.sample(num_requests).values,dtype="float")
+        lrequests = np.flip(vals,axis=1)
+        np.savetxt(f"requests_{num_requests}.txt", lrequests)
+
+if __name__ == "__main__":
+
+    requests = pandas.read_csv("data/worldcities.csv", sep=",",usecols=["lat","lng"])
+
+    for num_requests in [10, 50, 100, 500, 1000]:
+        sample_requests(requests, num_requests)
