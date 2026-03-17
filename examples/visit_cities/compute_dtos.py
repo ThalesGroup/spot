397057a6d7ab78b1e3823075731fb601c9e77cdd
michelnowak-thales <michel.nowak@thalesgroup.com>
2026-03-17 13:24:04 +0100

    Updated DTOs

--- examples/visit_cities/compute_dtos.py
+++ examples/visit_cities/compute_dtos.py
@@ -16,30 +16,15 @@ def prepare_satellite(satellite_id):
                   "2 25544  51.6429  29.4166 0004559 104.3372 354.3186 15.54111847 84492"]
     
     # compute ephemeris
-    satellite.init_from_tle(tle)
-    
-    # save satellite
-    with open(f"satellite_{satellite_id}.sat","wb") as file:
-        pickle.dump(satellite, file)
+    satellite.init_from_tle(tle_string)
     
     return satellite
 
-def compute_dtos(satellite, requests, prefix="dtos"):
-    """
-    """
-    # compute data take opportunity
-    dtos = satellite.compute_data_take_opportunity(requests)
-
-    # save dtos
-    np.savetxt(f"{prefix}.txt", dtos)
-
-
 if __name__ == "__main__":
 
-    for num_requests in [10, 50, 100, 500, 1000]:
-
-        # read requests
-        requests = np.genfromtxt(f"requests_{num_requests}.txt")
-        for satellite_id in [1, 5, 10, 15]:
+    for num_requests in [50]:
+        requests = np.genfromtxt(f"data/requests_{num_requests}.txt")
+        for satellite_id in [1, 2, 3, 4, 5]:
             satellite = prepare_satellite(satellite_id)
-            compute_dtos(requests, satellite, prefix="dtos_{num_requests}_{satellite_id}")
+            dtos = satellite.compute_data_take_opportunity(requests)
+            np.savetxt(f"data/dtos_{num_requests}_{satellite_id}.txt", dtos)
