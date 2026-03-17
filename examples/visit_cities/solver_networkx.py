f4a80f5804e88811515f42cb2ac36ed80142aeef
michelnowak-thales <michel.nowak@thalesgroup.com>
2026-03-17 13:24:59 +0100

    Updated example

--- examples/visit_cities/solver_networkx.py
+++ examples/visit_cities/solver_networkx.py
@@ -2,51 +2,59 @@ import numpy as np
 import pickle
 
 
+from spot.rl.environments.satellite import SatelliteEnvironment
 from spot.mis.networkx import NetworkXSolver
 
 
-def solve_networkx(requests, satellites, delta_t):
+def solve(requests, satellites, delta_t):
     """ Solves the problem by using the Maximum Independent Set with Networkx
         building solver
     """
 
+    priorities = np.ones(len(requests))
+
     # read DTOs
     dtos = []
     for satellite_id, _ in enumerate(satellites):
-        dto = np.genfromtxt(f"data/dtos_{len(requests)}_{satellite_id}.txt", delimiter=" ")
+        dto = np.genfromtxt(f"data/dtos_{len(requests)}_{satellite_id+1}.txt", delimiter=" ")
         dtos.append(dto)
     
     # prepare solver
-    sovler_params={
-        "prefix": f"nx_{len(requests)}_{len(satellites)}",
+    solver_params={
+        "prefix": f"nx_{delta_t}_{len(requests)}_{len(satellites)}",
         "delta_t": delta_t,
     }
+    
     solver = NetworkXSolver(solver_params)
-
     # run solver
     solver.run(
         dtos,
         requests,
         priorities,
-        satellites),
+        satellites,
+        save_graphs=True,
+        plot_graphs=True),
 
 
 if __name__ == "__main__":
 
-    num_requests = 100
-    num_satellites = 10
+    num_requests = 50
+    num_satellites = 5
     delta_t = 10
+
+    # ISS station
+    tle_string = ["1 25544U 98067A   17314.40254821  .00006490  00000-0  10525-3 0 9997",
+                  "2 25544  51.6429  29.4166 0004559 104.3372 354.3186 15.54111847 84492"]
 
     # load satellites
     satellites = []
-    for sat_id in range(num_satellites):
-        with open(f"data/satellite_{sat_id}.sat","rb") as file:
-            satellite = pickle.load(file)
-            satellite.init_from_tle()
-            satellites.append(satellite)
+    for satellite_id in [1, 2, 3]:
+        satellite = SatelliteEnvironment(satellite_id)
+        satellite.init_from_tle(tle_string)
+        satellites.append(satellite)
 
     # read requests
     requests = np.genfromtxt(f"data/requests_{num_requests}.txt")
 
     # solve problem
-    solve_networkx(requests, satellites, delta_t)
+    solve(requests, satellites, delta_t)
