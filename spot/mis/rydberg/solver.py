6473adc112ef586266c7f025a0da127524d18b22
michelnowak-thales <michel.nowak@thalesgroup.com>
2026-03-18 10:11:16 +0100

    Update backends in rydberg solvers

--- spot/mis/rydberg/solver.py
+++ spot/mis/rydberg/solver.py
@@ -3,13 +3,18 @@
 # Author : Michel Nowak <michel.nowak@thalesgroup.com>
 # Date   : 16.03.2026
 
+from dataclasses import replace
 import qoolqit
 import pulser
 from pulser.devices import AnalogDevice, MockDevice
+from pulser_pasqal import PasqalCloud
 from pulser_pasqal.backends import EmuMPSBackend
+
+from qubosolver.config import SolverConfig, QPU, RemoteEmulator
 
 from spot.mis.solver import Solver
 from spot.mis.rydberg.cloud import PasqalCloudManager
+
 
 
 class RydbergSolver(Solver):
@@ -28,46 +33,61 @@ class RydbergSolver(Solver):
         manager = PasqalCloudManager()
 
         connection = PasqalCloud(
+            project_id=manager.project_id,
             username=manager.username,
             password=manager.password,
-            project_id=manager.project_id,
         )
         return connection
 
     def get_backend(self, seq):
-        """ backend_type : "local_emulator", "emu_mps_remote_emulator", "fresnel_can1"
+        """ get backend for pulse solver
         """
         if self.backend_type == "local_emulator":
             backend = pulser.backends.QutipBackendV2(seq)
 
         elif self.backend_type=="emu_mps_remote_emulator":
 
-            config = pulser.backend.BackendConfig(
-                default_num_shots=self.num_shots)
             backend = pulser.backends.EmuMPSBackend(
                 seq,
-                connection,
-                config)
+                connection=self.get_connection())
 
         elif self.backend_type == "fresnel_can1":
 
-            config = pulser.backend.BackendConfig(
-                default_num_shots=self.num_shots)
             backend = pulser.backends.QPUBackend(
                 seq,
-                connection,
-                config=config)
+                connection=self.get_connection())
 
         return backend
 
     def get_device(self):
-        """ Gets device
+        """ Gets device for pulse solver
         """
-        if self.backend_type=="local_emulator" or "emu_mps_remote_emulator":
-            # device = replace(AnalogDevice.to_virtual(), max_radial_distance=900)
+        if self.backend_type=="local_emulator":
             device = MockDevice
+        elif self.backend_type=="emu_mps_remote_emulator":
+            device = AnalogDevice
+        elif self.backend_type=="fresnel_can1":
+            device = self.get_connection().fetch_available_devices()["FRESNEL_CAN1"]
+        return device
 
-        elif self.bakcend_type=="fresnel_can1":
+    def get_qubo_backend(self):
+        """
+        """
+        return QPU(connection=self.get_connection(), runs=self.num_shots)
+
+    def get_qubo_device(self):
+        """
+        """
+        if self.backend_type=="emu_mps_remote_emulator":
+            device = replace(AnalogDevice.to_virtual(), max_radial_distance=900)
+            device = RemoteEmulator(
+                 backend_type = EmuMPSBackend,
+                 connection = self.get_connection()
+             )
+        elif self.backend_type=="fresnel_can1":
             device = qoolqit.devices.Device(
                     pulser_device=self.get_connection().fetch_available_devices()["FRESNEL_CAN1"])
         return device
+
+
+
